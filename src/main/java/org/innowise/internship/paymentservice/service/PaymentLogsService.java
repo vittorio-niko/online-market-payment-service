9b06b95e1c04ba4f9ac02cb9c459da559ed4f09b
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-03-12 21:21:14 +0300

    feat: add services for creating payment logs

---
+++ src/main/java/org/innowise/internship/paymentservice/service/PaymentLogsService.java
@@ -0,0 +1,29 @@
+package org.innowise.internship.paymentservice.service;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.innowise.internship.paymentservice.model.dto.log.request.CreatePaymentLogRequestDto;
+import org.innowise.internship.paymentservice.model.entity.PaymentLog;
+import org.innowise.internship.paymentservice.model.mapper.PaymentLogMapper;
+import org.innowise.internship.paymentservice.repository.PaymentLogsRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentLogsService {
+    private final PaymentLogsRepository paymentLogsRepository;
+    private final PaymentLogMapper paymentLogMapper;
+
+    @Transactional
+    public PaymentLog createPaymentLog(@NonNull CreatePaymentLogRequestDto dto) {
+        PaymentLog log = paymentLogMapper.toPaymentLog(dto);
+        log.setTimestamp(Instant.now());
+        log.setPaymentId(UUID.randomUUID().toString());
+
+        return paymentLogsRepository.save(log);
+    }
+}
