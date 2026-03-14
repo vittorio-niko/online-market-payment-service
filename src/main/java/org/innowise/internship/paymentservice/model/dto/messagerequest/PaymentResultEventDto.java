14e8e287ea7e7280c0bbd965b0f5b98229ed5e98
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-03-14 15:23:53 +0300

    feat: add dto for inbox and outbox requests creation

---
+++ src/main/java/org/innowise/internship/paymentservice/model/dto/messagerequest/PaymentResultEventDto.java
@@ -0,0 +1,27 @@
+package org.innowise.internship.paymentservice.model.dto.messagerequest;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.innowise.internship.paymentservice.model.entity.outbox.PaymentOutboxStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentResultEventDto {
+    @NotBlank
+    private String paymentId;
+
+    @NotNull
+    private Long orderId;
+
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    private String status;
+}
