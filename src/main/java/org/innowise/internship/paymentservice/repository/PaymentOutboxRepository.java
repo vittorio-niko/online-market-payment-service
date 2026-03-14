f4924b73af3238dbaedbfccf126a6b798c97296d
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-03-14 15:22:17 +0300

    feat: add repositories for inbox and outbox

---
+++ src/main/java/org/innowise/internship/paymentservice/repository/PaymentOutboxRepository.java
@@ -0,0 +1,16 @@
+package org.innowise.internship.paymentservice.repository;
+
+import org.innowise.internship.paymentservice.model.entity.outbox.PaymentOutboxRequest;
+import org.innowise.internship.paymentservice.model.entity.outbox.PaymentOutboxStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentOutboxRepository
+        extends MongoRepository<PaymentOutboxRequest, String> {
+
+    List<PaymentOutboxRequest> findAllByStatus(PaymentOutboxStatus status, Pageable pageable);
+}
