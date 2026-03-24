94aaeb354be70273ed41797c20af98a3662cb8db
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-03-24 23:30:42 +0300

    refactor: add inbox records processor

--- src/main/java/org/innowise/internship/paymentservice/scheduler/PaymentInboxScheduler.java
+++ src/main/java/org/innowise/internship/paymentservice/scheduler/PaymentInboxScheduler.java
@@ -1,13 +1,9 @@
 package org.innowise.internship.paymentservice.scheduler;
 
 import lombok.RequiredArgsConstructor;
-import org.innowise.internship.paymentservice.bankclient.BankClient;
-import org.innowise.internship.paymentservice.bankclient.BankPaymentStatus;
 import org.innowise.internship.paymentservice.model.entity.inbox.PaymentInboxRequest;
-import org.innowise.internship.paymentservice.model.entity.inbox.PaymentInboxStatus;
-import org.innowise.internship.paymentservice.model.entity.log.PaymentStatus;
+import org.innowise.internship.paymentservice.service.messageservice.PaymentInboxProcessor;
 import org.innowise.internship.paymentservice.service.messageservice.PaymentInboxService;
-import org.innowise.internship.paymentservice.service.orchestratorservice.PaymentOrchestratorService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,29 +13,15 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class PaymentInboxScheduler {
-    private final PaymentOrchestratorService paymentOrchestratorService;
+    private final PaymentInboxProcessor paymentInboxProcessor;
 
-    private final BankClient client;
     private final PaymentInboxService paymentInboxService;
 
     @Scheduled(fixedDelayString = "${app.kafka.inbox.processing-delay}")
     void processInboxRecords() {
         List<PaymentInboxRequest> inboxPaymentRecords = paymentInboxService.getInboxRecordsBatchForProcessing();
         for (var record : inboxPaymentRecords) {
-            try {
-                // mock bank client call
-                BankPaymentStatus status = client.processPayment(
-                        record.getUserId(), record.getPaymentAmount()
-                );
-
-                if (status == BankPaymentStatus.SUCCESS) {
-                    paymentOrchestratorService.finalizePayment(record, PaymentStatus.SUCCESS);
-                } else {
-                    paymentOrchestratorService.finalizePayment(record, PaymentStatus.FAILURE);
-                }
-            } catch (Exception e) {
-                paymentOrchestratorService.markAsFailed(record);
-            }
+            paymentInboxProcessor.processInboxRecord(record);
         }
     }
 
---
+++ src/main/java/org/innowise/internship/paymentservice/service/messageservice/PaymentInboxProcessor.java
@@ -0,0 +1,35 @@
+package org.innowise.internship.paymentservice.service.messageservice;
+
+import lombok.RequiredArgsConstructor;
+import org.innowise.internship.paymentservice.bankclient.BankClient;
+import org.innowise.internship.paymentservice.bankclient.BankPaymentStatus;
+import org.innowise.internship.paymentservice.model.entity.inbox.PaymentInboxRequest;
+import org.innowise.internship.paymentservice.model.entity.log.PaymentStatus;
+import org.innowise.internship.paymentservice.service.orchestratorservice.PaymentOrchestratorService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentInboxProcessor {
+    private final PaymentOrchestratorService paymentOrchestratorService;
+
+    private final BankClient client;
+
+    public void processInboxRecord(PaymentInboxRequest record) {
+        BankPaymentStatus status;
+        try {
+            status = client.processPayment(
+                    record.getUserId(), record.getPaymentAmount()
+            );
+        } catch (Exception e) {
+            paymentOrchestratorService.markAsFailed(record);
+            return;
+        }
+
+        if (status == BankPaymentStatus.SUCCESS) {
+            paymentOrchestratorService.finalizePayment(record, PaymentStatus.SUCCESS);
+        } else {
+            paymentOrchestratorService.finalizePayment(record, PaymentStatus.FAILURE);
+        }
+    }
+}
