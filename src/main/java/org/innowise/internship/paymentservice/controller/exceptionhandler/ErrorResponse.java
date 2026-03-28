fed5e0674c3da10635f1e1e0781577d3a55446cc
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-03-28 17:47:46 +0300

    feat: add exception handling for controller

---
+++ src/main/java/org/innowise/internship/paymentservice/controller/exceptionhandler/ErrorResponse.java
@@ -0,0 +1,21 @@
+package org.innowise.internship.paymentservice.controller.exceptionhandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private ErrorCode code;
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> fieldErrors;
+}
