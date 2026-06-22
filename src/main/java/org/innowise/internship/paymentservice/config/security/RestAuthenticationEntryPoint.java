5d7490c8bf9ae0ed08dd591b72173fc5644f6443
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-06-22 20:19:00 +0300

    feat: enable jwt-based protection for endpoints

---
+++ src/main/java/org/innowise/internship/paymentservice/config/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package org.innowise.internship.paymentservice.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.innowise.internship.paymentservice.controller.exceptionhandler.ErrorCode;
+import org.innowise.internship.paymentservice.controller.exceptionhandler.ErrorResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(ErrorCode.UNAUTHORIZED)
+                .message("Authentication is required to access this resource")
+                .build();
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
